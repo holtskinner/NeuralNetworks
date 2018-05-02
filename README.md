8e0b01e16954b4b217c8acb3483cd119e816b1fd
HoltWasHere <holtwashere@gmail.com>
2018-05-02 15:34:55 -0500

    Added Homemade Image CLassification with Keras

--- README.md
+++ README.md
@@ -2,20 +2,30 @@
 ### Introduction
 Neural Networks and Deep Learning are the biggest buzzwords in Computer Science today. One of the particular fields that shows a great deal of promise are Convolutional Neural Networks or CNN's these special neural networks use a process known as convolution to learn features in conjunction with traditional Back Propogation. CNNs are particularly useful for processing images because convolution itself treats the data as a 2D matrix. Many different pretrained models exist to process image recognition and they each provide different levels of accuracy and different specialties. There are also a number of APIs featuring pretrained networks to allow more developers to utilize image recognition technology. For this project, these different models and APIs will be compared and contrasted to determine the best use cases.
 
+### Dataset
+For simplicity purposes, all classifiers will be trained with a simple dataset that consists of cats and dogs. The classifiers will be analyzed based on their ability to differentiate between the two different creatures. The training set imcludes 8000 images, 4000 cats and 4000 dogs. The test set contains 2000 images, 1000 cats and 1000 dogs. While this dataset is simple, the purpose is to learn the process of the CNN and the basic construction, as well as comparing the models.
+
 ### Technologies Used
 Python
 TensorFlow
 Keras
 Numpy
 
+Google Cloud
+
+All computation was performed on a Google Cloud Compute Instance running Ubuntu 18 to speed up computation.
+
 ### Homemade Image Classification with Keras
 
-Dataset - CIFAR10
-This Training
+For the first attempt at image classification, 
 
 
 ### Resnet50
 
 The first attempt 
+
+### Google Cloud Vision API
+
+### Amazon Rekognition
 
 ### Results
--- homemade.py
+++ homemade.py
@@ -1,28 +1,104 @@
+# Initialize Neural Network as Sequential
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
+# Perform Convolution Step on Training Images
+# Use Max in Pooling Operation
+# Flatten Arrays into a linear vector
+# Dense is Fully Connected (Regular MLP)
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+
+# Image Preprocessor to prevent overfitting
+from keras.preprocessing.image import ImageDataGenerator
+
+import numpy as np
+from keras.preprocessing import image
 
 
-def createModel():
-    model = Sequential()
-    model.add(Conv2D(32, (3, 3), padding='same',
-                     activation='relu', input_shape=input_shape))
-    model.add(Conv2D(32, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
+def create_model():
 
-    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
+    # Instansiate Network
+    classifier = Sequential()
 
-    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
+    # Convolution Layer 32 Filters 3x3 each, each image 64*64 RGB Relu Function
+    classifier.add(
+        Conv2D(32, (3, 3), input_shape=(64, 64, 3), activation='relu'))
 
-    model.add(Flatten())
-    model.add(Dense(512, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(nClasses, activation='softmax'))
+    # Pooling Layer to reduce size of image
+    classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
-    return model
+    classifier.add(Flatten())
+
+    # Hidden Fully Connected Layer
+    classifier.add(Dense(units=128, activation='relu'))
+
+    # Output Layer Gives Binary Output (Cat or Dog)
+    classifier.add(Dense(units=1, activation='sigmoid'))
+
+    # Optimizer - Use Stochastic Gradient Decent, Use Cross Entropy Loss Function, Measure Accuracy
+    classifier.compile(
+        optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    return classifier
+
+
+def train_classifier(classifier):
+
+    # Creates extra training data by flipping, rotating images
+    train_datagen = ImageDataGenerator(rescale=1. / 255,
+                                       shear_range=0.2,
+                                       zoom_range=0.2,
+                                       horizontal_flip=True)
+
+    test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+    training_set = train_datagen.flow_from_directory('training_set',
+                                                     target_size=(64, 64),
+                                                     batch_size=32,
+                                                     class_mode='binary')
+
+    training_set.class_indices
+    test_set = test_datagen.flow_from_directory('test_set',
+                                                target_size=(64, 64),
+                                                batch_size=32,
+                                                class_mode='binary')
+
+    classifier.fit_generator(training_set,
+                             steps_per_epoch=8000,
+                             epochs=25,
+                             validation_data=test_set,
+                             validation_steps=2000)
+
+    return classifier
+
+
+def predict(classifier, image_path):
+
+    test_image = image.load_img(image_path, target_size=(64, 64))
+
+    test_image = image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis=0)
+
+    result = classifier.predict(test_image)
+
+    if result[0][0] == 1:
+        prediction = 'dog'
+    else:
+        prediction = 'cat'
+
+    return prediction
+
+
+def main():
+    classifier = create_model()
+    classifier = train_classifier(classifier)
+
+    classifier.save("model.h5")
+    prediction = predict(classifier, "./Cat.jpg")
+
+    print(prediction)
+    f = open('prediction.txt', 'w')
+
+    f.write(prediction)
+    f.close()
+
+
+main()
